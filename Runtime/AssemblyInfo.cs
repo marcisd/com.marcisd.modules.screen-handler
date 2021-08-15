dab3e9cfefd79e323899d06ac688f793d66ca095
Marci San Diego <marcianosd@dm-ed.com>
2021-08-15 14:33:16 +0800

    Updated assembly naming

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("com.marcisd.modules.screen-handler.Editor")]
+[assembly: InternalsVisibleTo("MSD.Modules.ScreenHandler.Editor")]
